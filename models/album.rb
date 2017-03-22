dc88f18fec05b8047cbe3857975ec0929db5866d
Eun-Joo <eunjooyoon@hotmail.co.uk>
2017-03-22 16:06:05 +0000

    finished all exercises after checking they work

--- models/album.rb
+++ models/album.rb
@@ -10,20 +10,55 @@ class Album
   def initialize(options)
     @id = options['id'].to_i
     @title = options['title']
-    @genre = options['genre'].to_i
+    @genre = options['genre']
     @artist_id = options['artist_id'].to_i
   end
 
   def save()
-    sql = "INSERT INTO music_collection (
+    sql = "INSERT INTO albums (
     title,
     genre,
     artist_id
     ) VALUES (
     '#{@title}',
-    '#{genre}',
-    #{artist_id}) RETURNING id;"
+    '#{@genre}',
+    #{@artist_id}) RETURNING *"
     @id = SqlRunner.run(sql).first()['id'].to_i
   end
+
+    def delete()
+      sql = "DELETE FROM albums WHERE id = #{@id}"
+      SqlRunner.run(sql)
+    end
+
+    def update()
+      sql = "UPDATE albums 
+        SET (
+        title, 
+        genre,
+        artist_id) = ( 
+        '#{@title}', 
+        #{@genre}),
+        #{@artist_id}
+        WHERE id = #{@id}"
+      SqlRunner.run(sql)
+    end
+
+    def self.all()
+      sql = "SELECT * FROM albums"
+      result = SqlRunner.run(sql)
+      return result.map{|album| Album.new(album)}
+    end
+
+    def self.delete_all()
+      sql = "DELETE FROM albums"
+      SqlRunner.run(sql)
+    end
+
+    def artist()
+      sql = "SELECT * FROM artists WHERE artist_id = #{@artist_id}"
+      result = SqlRunner.run(sql)
+      return result.map{|artist| Artist.new(artist)}
+    end
 
 end
--- models/artist.rb
+++ models/artist.rb
@@ -7,14 +7,32 @@ class Artist
   attr_reader :id, :name
   
   def initialize(options)
-    @id = options['id'].to_i
+    @id = options['artist_id'].to_i
     @name = options['name']
   end
 
   def save()
     sql = "INSERT INTO artists (name)
     VALUES ('#{name}') RETURNING *"
-    @id = SqlRunner.run(sql).first()['id'].to_i
+    @id = SqlRunner.run(sql).first()['artist_id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM artists"
+    result = SqlRunner.run(sql)
+    artists = result.map{|artist| Artist.new(artist)}
+    return artists
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM artists"
+    SqlRunner.run(sql)
+  end
+
+  def albums()
+    sql = "SELECT * FROM albums WHERE artist_id = #{id}"
+    result = SqlRunner.run(sql)
+    return result.map{|album| Album.new(album)}
   end
 
 end
